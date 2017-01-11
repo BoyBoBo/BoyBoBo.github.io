2066ea5e27d40bc22e06c75abe8d9648295be683
bobo <929556453@qq.com>
2017-01-11 23:34:02 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,53 @@
+var generateBtn = document.getElementById('generate_btn');
+var container = document.getElementById('container');
+
+var LEN = 5;
+
+var endTime, startTime;
+
+generateBtn.addEventListener('click', function(){
+	startTime = 0, endTime = 0;
+	container.innerHTML = '';
+	var arr = [];
+	for(var i = 0; i < LEN * LEN; i ++) {
+		arr.push(i + 1);
+	}
+	var randomArr = [];
+	do {
+		var index = Math.floor(Math.random() * arr.length) + 1;
+		var num = arr.splice(index-1, 1);
+		console.log(num);
+		randomArr.push(num);
+	} while(arr.length > 0);
+	
+	for(var i = 0; i < LEN; i ++) {
+		var ulEle = document.createElement('ul');
+		for(var j = 0; j < LEN; j ++) {
+			var ele = document.createElement('li');
+			ele.innerText = randomArr[i * LEN + j];
+			ulEle.appendChild(ele);
+		}
+		container.appendChild(ulEle);
+	}
+});
+
+var expect = 1;
+
+container.addEventListener('click', function(e){
+	var target = e.target;
+	if(target.constructor === HTMLLIElement){
+		var num = Number(target.innerText);
+		if(num === expect) {
+			target.style.backgroundColor = 'green';
+			if(expect === LEN * LEN){
+				endTime = new Date().getTime();
+				alert('共用时：' + ((endTime - startTime) / 1000) + 's');
+			} else if(expect === 1){
+				startTime = new Date().getTime();
+				expect ++;
+			} else {
+				expect ++;
+			}
+		}
+	}
+});
